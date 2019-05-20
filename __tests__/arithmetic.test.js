3a9636c4565bbe0ffe0fa1cda2194cbe755fbd8e
Matt Wilkin <mwilkin22@gmail.com>
2019-05-20 16:42:30 -0700

    establish multiplication and division functions.

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,8 +1,21 @@
 'use strict';
 
+var faker = require('faker');
+
+var randomNumber = faker.random.number();
+
+
 const arithmetic = require('../lib/arithmetic.js');
 
-// describe('Arithmetic Module', () => {
-//   it('requires ')
+describe('Arithmetic Module', () => {
+  it('should add two numbers', () =>{
+    let x = randomNumber;
+    let y = randomNumber;
+    let sum1 = x + y;
 
-// });
+    var sum2 = arithmetic.add(x, y);
+
+    expect(sum2).to.be.equal(sum1);
+  });
+
+});
--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const math = require('./lib/arithmetic.js');
 console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+console.log(math.multiply(3,3)); // 9
+console.log(math.divide(12,2)); // 6
